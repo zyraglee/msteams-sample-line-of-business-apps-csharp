81004b11a5b9afdfed890227845b5c1ec53fc3f4
Arun Kumar Anaparthi <v-aruana@microsoft.com>
2018-08-29 17:12:20 +0530

    InventoryBot changes

---
+++ CommonApps/InventoryBot/Helper/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Microsoft.Teams.Samples.HelloWorld.Web.Helper
+{
+    public class Constants
+    {
+        public const string Industry = "Industry";
+        public const string Rebook = "Rebook the passenger";
+        public const string newInventoryCount = "Add Invenotry";
+        public const string BlockInventory = "Lock Inventory";
+        public const string RetireInventory = "Inactive Invenotry";
+        public const string RequestNewStock = "Request New Stock";
+    }
+}
