6807534fd5fad4cb4a6459049060731acc763f0a
Arun Kumar Anaparthi <v-aruana@microsoft.com>
2018-08-31 16:39:45 +0530

    Flight Info bot changes

--- Airline/FlightInfo/Helper/O365CardHelper.cs
+++ Airline/FlightInfo/Helper/O365CardHelper.cs
@@ -21,24 +21,24 @@ namespace Microsoft.Teams.Samples.HelloWorld.Web.Helper
             int count = 0;
             foreach (var flight in flights)
             {
-                DateTime journeydate = TimeZoneInfo.ConvertTimeToUtc(JourneyDate.Date);
-                DateTime dabasedate = TimeZoneInfo.ConvertTimeToUtc(flight.JourneyDate.Date);
+                DateTime journeydate = JourneyDate.Date.AddDays(1);
+                DateTime dabasedate =(flight.JourneyDate.Date);
                 if (journeydate.Date == dabasedate.Date)
                 {
-                    listCard.content.title = "The following flights are avilibile on " + flight.JourneyDate.ToShortDateString();
+                    listCard.content.title = "The following flights are available on " + flight.JourneyDate.ToShortDateString();
                     count = 1;
                     var item = new Item();
-                    item.icon = "https://airlinebaggage.azurewebsites.net/resources/Flight.png";
+                    item.icon = "https://flightinfobot.azurewebsites.net/resources/Airline-Flight-Bot-03.png";
                     item.type = "resultItem";
                     item.id = flight.FlightNumber;
                     item.title = "Flight: " + flight.FlightNumber + "   |" + " Departure: " + flight.Departure.ToShortTimeString() + "   |" + " Arrival: " + flight.Arrival.ToShortTimeString();
-                    item.subtitle = "PNR: " + flight.FromCity + "   |" + " To: " + flight.ToCity + "  |" + " Seats:  " + flight.SeatCount;
+                    item.subtitle = "From: " + flight.FromCity + "   |" + " To: " + flight.ToCity + "  |" + " Seats:  " + flight.SeatCount;
 
                     item.tap = new Tap()
                     {
                         type = "imBack",
                         title = "FlightNumber",
-                        value = "Show Flights by Number " + " (" + flight.FlightNumber + ")"
+                        value = "Show details of flight" + " (" + flight.FlightNumber + ")"
                     };
                     list.Add(item);
                 }
@@ -46,7 +46,7 @@ namespace Microsoft.Teams.Samples.HelloWorld.Web.Helper
             }
             if (count == 0)
             {
-                listCard.content.title = "Flights not avilibile for selected date. Please check some other date";
+                listCard.content.title = "Flights not available for selected date. Please check some other date";
             }
             listCard.content.items = list.ToArray();
 
@@ -61,14 +61,14 @@ namespace Microsoft.Teams.Samples.HelloWorld.Web.Helper
             {
                 ActivityTitle = $"Flight number: **{flight.FlightNumber}**",
                 ActivitySubtitle = $"PNR: **{flight.PNR}**",
-                ActivityImage = "https://airlinebaggage.azurewebsites.net/resources/Flight.png",
+                ActivityImage = "https://flightinfobot.azurewebsites.net/resources/Airline-Flight-Bot-03.png",
                 Facts = new List<O365ConnectorCardFact>
                     {
 
                         new O365ConnectorCardFact("From", flight.FromCity),
                         new O365ConnectorCardFact("To", flight.ToCity),
                         new O365ConnectorCardFact("Date of Journey",flight.JourneyDate.ToShortDateString())
-                        
+
                     }
             };
             var PNRNumberCard = new O365ConnectorCardActionCard(
@@ -92,32 +92,10 @@ namespace Microsoft.Teams.Samples.HelloWorld.Web.Helper
                         O365ConnectorCardHttpPOST.Type,
                         "Rebook the Passenger",
                         Constants.Rebook,
-                        @"{""Value"":""{{pnrNumberInput.value}}""}")
+                        @"{""pnrNumberInput"":""{{pnrNumberInput.value}}"", ""flightNumberInput"":"+ flight.FlightNumber+ " }")
                 });
 
-            var PNRNumberCard1 = new O365ConnectorCardActionCard(
-                O365ConnectorCardActionCard.Type,
-                "Rebook the Passenger",
-                "PNR",
-                new List<O365ConnectorCardInputBase>
-                {
-                    new O365ConnectorCardTextInput(
-                        O365ConnectorCardTextInput.Type,
-                        "pnrNumberInput",
-                        true,
-                        "Enter PNR number",
-                        null,
-                        false,
-                        null)
-                },
-                new List<O365ConnectorCardActionBase>
-                {
-                    new O365ConnectorCardHttpPOST(
-                        O365ConnectorCardHttpPOST.Type,
-                        "Rebook the Passenger1",
-                        Constants.Rebook,
-                        @"{""Value"":""{{pnrNumberInput.value}}""}")
-                });
+
 
 
             O365ConnectorCard card = new O365ConnectorCard()
@@ -127,13 +105,12 @@ namespace Microsoft.Teams.Samples.HelloWorld.Web.Helper
                 Sections = new List<O365ConnectorCardSection> { section },
                 PotentialAction = new List<O365ConnectorCardActionBase>
                 {
-                    PNRNumberCard,
-                    PNRNumberCard1
+                    PNRNumberCard
 
                 }
 
 
-                };
+            };
             return card;
         }
     }
@@ -151,6 +128,12 @@ namespace Microsoft.Teams.Samples.HelloWorld.Web.Helper
     public class O365BodyValue
     {
         public string Value { get; set; }
+    }
+
+    public class RebookClass
+    {
+        public string flightNumberInput { get; set; }
+        public string pnrNumberInput { get; set; }
     }
 
 }
