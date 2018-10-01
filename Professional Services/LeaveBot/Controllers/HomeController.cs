33e388c0376a33c801416911f95dddd8cec0bdf8
Wajeed-MSFT <v-washai@microsoft.com>
2018-10-01 13:11:59 +0530

    Added DB read write part.

--- Professional Services/LeaveBot/Controllers/HomeController.cs
+++ Professional Services/LeaveBot/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using Microsoft.Teams.Samples.HelloWorld.Web.Repository;
+using System.Web.Mvc;
+using Microsoft.Teams.Samples.HelloWorld.Web.Models;
+using System;
+using System.Threading.Tasks;
 
 namespace Microsoft.Teams.Samples.HelloWorld.Web.Controllers
 {
@@ -11,8 +15,53 @@ namespace Microsoft.Teams.Samples.HelloWorld.Web.Controllers
         }
 
         [Route("hello")]
-        public ActionResult Hello()
+        public async Task<ActionResult> Hello()
         {
+            var emp = new Employee()
+            {
+                Name = "Wajeed",
+                EmailId = "v-washai@microsoft.com",
+                UserUniqueId = "Unique",
+                TenantId = "microsfttenatnaid",
+                DemoManagerEmailId = "v-washai@microsoft.com",
+                LeaveBalance = new LeaveBalance
+                {
+                    OptionalLeave = 2,
+                    PersonalLeave = 20,
+                    SickLeave = 10
+                },
+                ManagerEmailId = "v-washai@microsft.com",
+                PhotoPath = @"D:\test\updatedpath\photo.png",
+            };
+            var employeeDoc = await DocumentDBRepository.CreateItemAsync(emp);
+
+            var leave = new LeaveDetails()
+            {
+                AppliedByEmailId = "v-washai@micso.com",
+                LeaveId = "someuniqueId123456876543",
+                EmployeeComment = "Vacation",
+                LeaveType = LeaveType.PersonalLeave,
+                StartDate = new LeaveDate() { Date = DateTime.Now, Type = DayType.FullDay },
+                EndDate = new LeaveDate() { Date = DateTime.Now, Type = DayType.FullDay },
+                Status = LeaveStatus.PendingApproval,
+                ManagerComment = "You can tkae the leave"
+            };
+
+            var leaveDoc = await DocumentDBRepository.CreateItemAsync<LeaveDetails>(leave);
+
+            var AllEmployees = await DocumentDBRepository.GetItemsAsync<Employee>(e => e.Type == Employee.TYPE);
+
+            var AllLeave = await DocumentDBRepository.GetItemsAsync<LeaveDetails>(e => e.Type == LeaveDetails.TYPE);
+
+
+            var readEmployee = await DocumentDBRepository.GetItemAsync<Employee>("v-washai@microsoft.com");
+
+            var readLeave = await DocumentDBRepository.GetItemAsync<LeaveDetails>("someuniqueId123456876543");
+
+            readEmployee.LeaveBalance.OptionalLeave = 100;
+
+            var upatedEmp = await DocumentDBRepository.UpdateItemAsync<Employee>(readEmployee.EmailId, readEmployee);
+
             return View("Index");
         }
 
--- Professional Services/LeaveBot/Models/LeaveDetails.cs
+++ Professional Services/LeaveBot/Models/LeaveDetails.cs
@@ -1,34 +1,52 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace Microsoft.Teams.Samples.HelloWorld.Web.Models
 {
-    public class LeaveDetails
+    public class LeaveDetails 
     {
+        [JsonIgnore]
+        public const string TYPE = "LeaveDetails";
+
+        [JsonProperty(PropertyName = "type")]
+        public string Type { get; set; } = TYPE;
+
+        [JsonProperty(PropertyName = "id")]
         public string LeaveId { get; set; }
 
+        [JsonProperty(PropertyName = "appliedByEmailId")]
         public string AppliedByEmailId { get; set; }
 
+        [JsonProperty(PropertyName = "startDate")]
         public LeaveDate StartDate { get; set; }
 
+        [JsonProperty(PropertyName = "endDate")]
         public LeaveDate EndDate { get; set; }
 
+        [JsonProperty(PropertyName = "leaveType")]
         public LeaveType LeaveType { get; set; }
 
+        [JsonProperty(PropertyName = "employeeComment")]
         public string EmployeeComment { get; set; }
 
+        [JsonProperty(PropertyName = "managerComment")]
         public string ManagerComment { get; set; }
 
+        [JsonProperty(PropertyName = "status")]
         public LeaveStatus Status { get; set; }
 
     }
 
     public class LeaveDate
     {
+        [JsonProperty(PropertyName = "date")]
         public DateTime Date { get; set; }
-        public DayType LeaveType { get; set; }
+
+        [JsonProperty(PropertyName = "type")]
+        public DayType Type { get; set; }
     }
 
     public enum DayType
