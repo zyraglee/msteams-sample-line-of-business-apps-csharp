52c01746bd68a6370d1ddcdd0123b20fa00c31a4
Wajeed <v-washai@microsoft.com>
2018-10-09 17:59:03 +0530

    Updated deep links.

--- Professional Services/LeaveBot/Helpers/DeeplinkHelper.cs
+++ Professional Services/LeaveBot/Helpers/DeeplinkHelper.cs
@@ -8,13 +8,15 @@ namespace Microsoft.Teams.Samples.HelloWorld.Web.Helpers
 {
     public static class DeeplinkHelper
     {
-        public static string LeaveBoardDeeplink { get; set; } =
-            $"https://teams.microsoft.com/l/entity/{ApplicationSettings.AppId}/com.contoso.LeaveBot.leaveboard?webUrl={HttpUtility.UrlEncode(ApplicationSettings.BaseUrl)}&label=Leave%20Board";
+        public static string GetLeaveBoardDeeplink(string emailId)
+        {
+            return $"https://teams.microsoft.com/l/entity/{ApplicationSettings.AppId}/com.contoso.LeaveBot.leaveboard?webUrl={HttpUtility.UrlEncode(ApplicationSettings.BaseUrl + "?EmailId=" + emailId)}&label=Leave%20Board";
+        }
 
         public static string PublicHolidaysDeeplink { get; set; } =
-            $"https://teams.microsoft.com/l/entity/{ApplicationSettings.AppId}/com.contoso.LeaveBot.holidays?webUrl={HttpUtility.UrlEncode(ApplicationSettings.BaseUrl+"/first")}&label=Public%20Holidays";
+            $"https://teams.microsoft.com/l/entity/{ApplicationSettings.AppId}/com.contoso.LeaveBot.holidays?webUrl={HttpUtility.UrlEncode(ApplicationSettings.BaseUrl + "/first")}&label=Public%20Holidays";
 
         public static string HelpDeeplink { get; set; } =
-            $"https://teams.microsoft.com/l/entity/{ApplicationSettings.AppId}/com.contoso.LeaveBot.help?webUrl={HttpUtility.UrlEncode(ApplicationSettings.BaseUrl+"/second")}&label=Help";
+            $"https://teams.microsoft.com/l/entity/{ApplicationSettings.AppId}/com.contoso.LeaveBot.help?webUrl={HttpUtility.UrlEncode(ApplicationSettings.BaseUrl + "/second")}&label=Help";
     }
 }
